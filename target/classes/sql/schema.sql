82eeabe5b8eb6014e2ef870898afab266ee13415
Matheus França <gdaneluti@localhost.localdomain>
2019-04-04 22:31:35 -0300

    Novos arquivos

---
+++ target/classes/sql/schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.config (id, chave, valor) VALUES (1, 'URL_IP_VIGILANTE', 'https://ipvigilante.com/json/');
+INSERT INTO public.config (id, chave, valor) VALUES (2, 'URL_META_WEATHER_CIDADE', 'https://www.metaweather.com/api/location/search/?query=');
+INSERT INTO public.config (id, chave, valor) VALUES (3, 'URL_META_WEATHER_ID', 'https://www.metaweather.com/api/location/');
